f1b309e7d6572e59d2f06cb518453a5ed1870e41
massi-css <sebiha_massinissa@etu.univ-blida.dz>
2024-03-28 01:24:23 +0100

    fixed settings && color theme && add device forme

---
+++ pages/NewDevice.py
@@ -0,0 +1,39 @@
+import streamlit as st
+from utils.utils import *
+from widgets import sidebar
+
+# configure the page
+st.set_page_config(page_title="IOT Dashboard", layout="centered",initial_sidebar_state='expanded')
+
+#initialize the authentication status
+if 'authenticated' not in st.session_state:
+    st.session_state.authenticated = get_authentication_status()
+
+if st.session_state.authenticated == False:
+    st.switch_page("pages/login_page.py")
+elif st.session_state.authenticated == True:
+    # the sidebar
+    sidebar()
+    # back button 
+    if st.button("Back"):
+        st.switch_page("pages/Devices.py")
+    with st.form(key='ad_device_form'):
+        st.title("Add a new device")
+        device_name = st.text_input("Device name")
+        location = st.text_input("Location")
+        status = st.selectbox("Status", ["Active", "Inactive"])
+        longitude = st.number_input("Longitude")
+        latitude = st.number_input("Latitude")
+        submit = st.form_submit_button("Add device")
+        if submit:
+            st.success("Device added successfully")
+            data={
+                'Name': device_name,
+                'Location': location,
+                'Status': status,
+                'Longitude': longitude,
+                'Latitude': latitude
+            }
+            st.write(data)
+            st.session_state["device_num"] = -1
+            # st.rerun()
